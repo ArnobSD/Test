84093623882db82e0837ea2008278ecb06673769
Arnob Sutradhar <85068336+ArnobSD@users.noreply.github.com>
2021-08-12 11:00:48 -0700

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,23 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$phone = $_POST['phone'];
+$messege = $_POST['messege'];
+
+
+$email_form ='pcsdasdp@gmail.com';
+$email_subject ='New Form Submission';
+$email_body ="User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                "User Phone: $phone.\n".
+                "User Name: $messege.\n";
+
+
+$to ='pcsdasdp@gmail.com';
+$headers = "From: $email_from \r\n";
+$headers = "Reply-To: $visitor_email \r\n";
+
+mail($to, $email_subject, $email_body, $headers);
+
+header("Location: index.html")
+?>
