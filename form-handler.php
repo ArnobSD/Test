df528ba1a978ee7c658aa03a05a2c382fbe30e75
Arnob Sutradhar <85068336+ArnobSD@users.noreply.github.com>
2021-08-12 13:32:46 -0700

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -1,21 +1,22 @@
 <?php
+
 $name = $_POST["name"];
 $visitor_email = $_POST["email"];
 $phone = $_POST["phone"];
 $messege = $_POST["messege"];
 
 
-$email_form ="info@arnobsd.github.io";
+$email_form ="info@github.io";
 $email_subject ="New Form Submission";
-$email_body ="User Name: $name.\n".
+$email_body = "User Name: $name.\n".
                 "User Email: $visitor_email.\n".
                 "User Phone: $phone.\n".
                 "User Name: $messege.\n";
 
 
-$to ="pcsdasdp@gmail.com";
+$to ="arnobsdp@outlook.com";
 $headers = "From: $email_from \r\n";
-$headers = "Reply-To: $visitor_email \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
 
 mail($to, $email_subject, $email_body, $headers);
 
